9a9087559bd076b29f620b64678c65b158bf6b92
yuye-aws <yuyezhu@amazon.com>
2024-02-04 10:47:31 +0800

    remove search index tool switch case
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/providers/constants/index.ts
+++ src/providers/constants/index.ts
@@ -8,7 +8,6 @@ export const PROVIDERS = {
   PPL_GENERATOR: 'ppl_generator',
   ML_COMMONS: 'ml_commons',
   AGENT_FRAMEWORK: 'agent_framework',
-  SEARCH_INDEX_TOOL: 'search_index_tool',
 } as const;
 
 export const OPENSEARCH_CONFIG = {
